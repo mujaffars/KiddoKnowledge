689900ac7fcef69f086dd05474f81823b3f00ce1
mujaffars <mujaffar.sanadi01@gmail.com>
2024-07-19 22:12:34 +0530

    Progress loader implementation

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,19 +1,42 @@
+"use client";
+import React, { useState } from 'react';
 import Link from 'next/link';
 
 const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <nav className="bg-blue-500 p-4">
       <div className="container mx-auto flex justify-between items-center">
         <div className="text-white font-bold text-lg">
           <Link href="/">Kiddo Knowledge</Link>
         </div>
-        <div className="space-x-4">
+        <div className="hidden md:flex space-x-4">
           <Link href="/" className="text-white hover:text-gray-300">Home</Link>
           <Link href="/nursery" className="text-white hover:text-gray-300">Nursery</Link>
           <Link href="/first-grade" className="text-white hover:text-gray-300">1st Grade</Link>
           <Link href="/second-grade" className="text-white hover:text-gray-300">2nd Grade</Link>
         </div>
+        <div className="md:hidden">
+          <button onClick={toggleMenu} className="text-white focus:outline-none">
+            <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path>
+            </svg>
+          </button>
+        </div>
       </div>
+      {isMenuOpen && (
+        <div className="md:hidden mt-2 space-y-2">
+          <Link href="/" className="block text-white hover:text-gray-300">Home</Link>
+          <Link href="/nursery" className="block text-white hover:text-gray-300">Nursery</Link>
+          <Link href="/first-grade" className="block text-white hover:text-gray-300">1st Grade</Link>
+          <Link href="/second-grade" className="block text-white hover:text-gray-300">2nd Grade</Link>
+        </div>
+      )}
     </nav>
   );
 };
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,39 @@
 import '../styles/globals.css';
+import NProgress from 'nprogress';
+import 'nprogress/nprogress.css'; // Import nprogress styles
+
 import Navbar from '../components/navbar';
 
+NProgress.configure({ showSpinner: false, speed: 400, minimum: 0.25 });
+
 function MyApp({ Component, pageProps }) {
+
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChangeStart = () => {
+      NProgress.start();
+    };
+
+    const handleRouteChangeComplete = () => {
+      NProgress.done();
+    };
+
+    const handleRouteChangeError = () => {
+      NProgress.done();
+    };
+
+    router.events.on('routeChangeStart', handleRouteChangeStart);
+    router.events.on('routeChangeComplete', handleRouteChangeComplete);
+    router.events.on('routeChangeError', handleRouteChangeError);
+
+    return () => {
+      router.events.off('routeChangeStart', handleRouteChangeStart);
+      router.events.off('routeChangeComplete', handleRouteChangeComplete);
+      router.events.off('routeChangeError', handleRouteChangeError);
+    };
+  }, [router]);
+
   return (
     <>
       <Navbar />
